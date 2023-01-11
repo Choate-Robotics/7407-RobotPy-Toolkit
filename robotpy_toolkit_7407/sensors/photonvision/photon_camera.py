3bdde520cfe9834a1cb44bd191072d83b486dee3
Sidharth Rao <sidharthmrao@gmail.com>
2023-01-11 08:43:55 -0500

    Fix 'has targets' circular function.

--- robotpy_toolkit_7407/sensors/photonvision/photon_camera.py
+++ robotpy_toolkit_7407/sensors/photonvision/photon_camera.py
@@ -3,7 +3,7 @@ from robotpy_toolkit_7407.sensors.photonvision.photon_target import PhotonTarget
 from wpimath.geometry import Pose3d
 
 class PhotonCamera:
-    def __init__(self, name, poseRelativeToRobot: Pose3d, height=None, pitch=None):
+    def __init__(self, name: str, poseRelativeToRobot: Pose3d, height=None, pitch=None):
         self.camera = photonvision.PhotonCamera(cameraName=name)
         self.latest_best_target: PhotonTarget = None
         self.latest_targets_all: list[PhotonTarget] = None
@@ -12,7 +12,7 @@ class PhotonCamera:
         self.pitch = pitch
 
     def hasTargets(self):
-        return self.latest_best_target is not None
+        return self.camera.hasTargets()
 
     def refresh(self):  # Call at the beginning of every loop. Saves target for the loop to optimize lookup times.
         self.latest_targets_all = [PhotonTarget(target) for target in self.getLatestResult().getTargets()] if self.hasTargets() else None
