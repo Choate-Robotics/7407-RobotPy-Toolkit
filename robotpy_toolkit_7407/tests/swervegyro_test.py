26c7f1c101ad03d98a48b45c65e0d5b15f9ec086
Matthew Bardoe <mbardoe@gmail.com>
2023-06-08 22:17:32 -0400

    PigeonWrapper started

--- robotpy_toolkit_7407/tests/swervegyro_test.py
+++ robotpy_toolkit_7407/tests/swervegyro_test.py
@@ -1,5 +1,13 @@
-from robotpy_toolkit_7407.sensors.gyro import SwerveGyro, BaseGyro
+import pytest
 
+from unittest.mock import MagicMock
+from robotpy_toolkit_7407.sensors.gyro import SwerveGyro, BaseGyro, PigeonIMUGyro_Wrapper
+
+@pytest.fixture
+def pigeon()->PigeonIMUGyro_Wrapper:
+    gyro=PigeonIMUGyro_Wrapper(3)
+    gyro._gyro=MagicMock()
+    return gyro
 
 def test_swerve_gyro() -> None:
     # setup
@@ -8,3 +16,11 @@ def test_swerve_gyro() -> None:
     gyro = SwerveGyro()
     # assert
     assert isinstance(gyro, BaseGyro)
+
+def test_pigeon_gyro_wrapper_init(pigeon)->None:
+    # setup
+    gyro=PigeonIMUGyro_Wrapper(3)
+    # action
+    gyro.init(10)
+    # assert
+    assert gyro.get_robot_heading()==10
